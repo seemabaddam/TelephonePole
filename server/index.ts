15d4ce43915810a224bde9c8c5fb54a0b06b382f
Seema Baddam <sbaddam8@gatech.edu>
2026-05-27 19:10:23 +0200

    db connected

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import { connectDB } from './db';
 
 dotenv.config({ path: '../.env' });
 
@@ -10,8 +11,12 @@ const PORT = process.env.PORT ?? 3001;
 app.use(cors());
 app.use(express.json());
 
-// TODO Agent 2: connectDB()
 // TODO Agent 3: app.use('/api/events', eventsRouter)
+
+connectDB().catch((err) => {
+  console.error('Failed to connect to MongoDB:', err);
+  process.exit(1);
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
